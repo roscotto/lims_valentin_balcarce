30a9923ef122578f64a6420673e440eeab0f032c
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2025-01-20 11:23:15 -0300

    accessors y mutators de val_min y val_max parametros

--- app/Models/Parametro.php
+++ app/Models/Parametro.php
@@ -2,11 +2,46 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Parametro extends Model
 {
   // Nombre de la tabla
   protected $table = 'parametros';
-  
+
+
+  /**
+   * Método que devuelve los valores MÍNIMOS quitando los ceros a la derecha del número decimal
+   * Utilizando accessors y mutators
+   * @return Atribute $value
+   */
+  public function valorMin() : Attribute
+  {
+    return Attribute::make (
+      function ($value) {
+        return rtrim(rtrim($value, '0'), '.');
+      },
+      function ($value) {
+        return $value;
+      }
+    );
+  }
+
+  /**
+   * Método que devuelve los valores MÁXIMOS quitando los ceros a la derecha del número decimal
+   * Utilizando accessors y mutators
+   * @return Atribute $value
+   */
+  public function valorMax() : Attribute
+  {
+    return Attribute::make (
+      function ($value) {
+        return rtrim(rtrim($value, '0'), '.');
+      },
+      function ($value) {
+        return $value;
+      }
+    );
+  }
 }
