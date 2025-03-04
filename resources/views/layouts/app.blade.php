8a2ec4cebb4e3eb9d9555fadaa256d5384cbd668
Pedro Gonzalez <pedro.gch01@gmail.com>
2025-03-04 14:12:23 -0300

    aplico grid

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,12 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 lg:grid lg:grid-cols-5 lg:grid-rows-[90px] lg:gap-1">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white shadow">
+                <header class="bg-white shadow col-span-4">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
@@ -28,7 +28,7 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
+            <main class="bg-white col-span-4 row-span-4 col-start-2 row-start-2">
                 {{ $slot }}
             </main>
         </div>
